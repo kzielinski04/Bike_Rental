7511bd896baef8428fe89b43e3765ab96c1b9de3
Kacper Zieliński <zielinski_kacper@outlook.com>
2024-11-24 20:52:04 +0100

    feat: add rent_bike function

--- bike_rental.py
+++ bike_rental.py
@@ -15,6 +15,7 @@ import json, datetime, os, smtplib
    - Użyj funkcji `json.dump()` do zapisu danych w pliku.
 '''
 
+#This function calculates rental cost, depending on rental duration
 def calculate_cost(rental_duration:int)->int:
     match rental_duration:
         case rental_duration if isinstance(rental_duration, int) == False or rental_duration < 1:
@@ -22,4 +23,8 @@ def calculate_cost(rental_duration:int)->int:
         case 1:
             return 10
         case _:
-            return 10 + ((rental_duration - 1) * 5) 
+            return 10 + ((rental_duration - 1) * 5)
+        
+def rent_bike(customer_name:str, rental_duration:int):
+    rent = {"name" : customer_name, "duration" : rental_duration, "cost" : calculate_cost(rental_duration)}
+    return rent
